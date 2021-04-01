281e9b307b7f8cc7a560903c11f2782cdf00b503
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-04-01 12:33:10 +0200

    scaffolding event, myevent, joinevent

---
+++ ProjektuppgiftASP.NET/Pages/Events.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProjektuppgiftASP.NET.Data;
+using ProjektuppgiftASP.NET.Models;
+
+namespace ProjektuppgiftASP.NET.Pages
+{
+    public class EventsModel : PageModel
+    {
+        private readonly ProjektuppgiftASP.NET.Data.ApplicationDbContext _context;
+
+        public EventsModel(ProjektuppgiftASP.NET.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Event> Event { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Event = await _context.Event.ToListAsync();
+        }
+    }
+}
