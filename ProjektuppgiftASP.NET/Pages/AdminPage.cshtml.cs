024c971a36209dd23204310c3a7a47ba3b548c0d
svante1987 <svantepalsson1987@gmail.com>
2021-04-13 15:04:46 +0200

    fixar med admin sidan så att man ska kunna ändra användares roller.

--- ProjektuppgiftASP.NET/Pages/AdminPage.cshtml.cs
+++ ProjektuppgiftASP.NET/Pages/AdminPage.cshtml.cs
@@ -2,15 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProjektuppgiftASP.NET.Data;
+using ProjektuppgiftASP.NET.Models;
 
 namespace ProjektuppgiftASP.NET.Pages
 {
     public class AdminPageModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<MyUser> _userManager;
+        private readonly EventContext _context;
+        public AdminPageModel(
+            UserManager<MyUser> userManager,
+            EventContext context
+            )
         {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public List<MyUser> Users;
+        public async Task OnGet() { 
+            var users = await _userManager.Users.ToListAsync();
+
+        Users = users;
+        }
+
+        [BindProperty]
+        public bool CheckBox { get; set; }
+        public async Task OnPost(string? id)
+        {
+            if (!CheckBox)
+                await _userManager.RemoveFromRoleAsync(await _userManager.FindByIdAsync(id), "Organizer");
+            else
+                await _userManager.AddToRoleAsync(await _userManager.FindByIdAsync(id), "Organizer");
+
+            await OnGet();
         }
     }
 }
