876c696bb91009d20ed0cb983ca63e36fc87d27a
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-04-14 09:36:17 +0200

    organizer add Event cshtml-kod start

---
+++ ProjektuppgiftASP.NET/Pages/Organizer/AddEvent.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProjektuppgiftASP.NET.Data;
+using ProjektuppgiftASP.NET.Models;
+
+namespace ProjektuppgiftASP.NET.Pages.Organizer
+{
+    public class AddEventModel : PageModel
+    {
+        private readonly EventContext _context;
+
+        public AddEventModel(EventContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Event Event { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Event.Add(Event);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
